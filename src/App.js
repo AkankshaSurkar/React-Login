936a83582b3c814e210e3e8bcadc695eb305cd2b
AkankshaSurkar <akankshasfdc96@gmail.com>
2023-05-12 15:30:11 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, {useState,useEffect,Fragment} from 'react';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [isLoggedIn, setIsLoggedIn]= useState(false);
+
+  // useEffect(()=>{
+  //   const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+  //   if(storedUserLoggedInInformation ==='1'){
+  //     setIsLoggedIn(true);
+  //   }
+  // },[]);
+  const loginHandler = (email, password)=>{
+    setIsLoggedIn(true);
+  }
+const logoutHandler =()=>{
+  setIsLoggedIn(false);
+};
+return(
+<Fragment>
+     <MainHeader  isAuthenticated= {isLoggedIn} onLogout={logoutHandler}/>
+    <main>
+      {!isLoggedIn && <Login onLogin={loginHandler}/>}
+      {isLoggedIn && <Home onLogout={logoutHandler}/>}
+      </main> 
+      </Fragment>
+);
+};
 
 export default App;
